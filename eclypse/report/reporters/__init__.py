89a48953cf6f8007535f8de7fb9c92f7a4eb12cb
Jacopo Massa <jacopomassa97@gmail.com>
2024-11-25 18:02:30 +0100

    docs: Fix reporters module docstring

--- eclypse/report/reporters/__init__.py
+++ eclypse/report/reporters/__init__.py
@@ -1,12 +1,14 @@
-"""Package for managing simulation reporters, including the off-the-shelf ones."""
+"""Package for managing simulation reporters, including the off-the-shelf ones.
+
+If you are interested in creating HTML reports, use the
+`eclypse-html-report <https://pypi.org/project/eclypse-html-report>` CLI tool.
+"""
 
 from typing import Dict, Type
 from eclypse_core.report.reporter import Reporter
 from .csv import CSVReporter
 from .gml import GMLReporter
 from .tensorboard import TensorBoardReporter
-
-__doc__ = Reporter.__doc__ if Reporter.__doc__ else ""
 
 
 def get_default_reporters() -> Dict[str, Type[Reporter]]:
