8101c19538fe7eafd1afef4bb5de8f3bf06ecd07
Jacopo Massa <jacopomassa97@gmail.com>
2026-04-01 17:43:43 +0200

    refactor: Simplify builder exports and restore link naming

--- eclypse/builders/application/__init__.py
+++ eclypse/builders/application/__init__.py
@@ -1,9 +1,4 @@
-"""Module for the application builders.
-
-It has the following builders:
-- SockShop: A microservice-based application that simulates an e-commerce platform, \
-    made of 7 microservices.
-"""
+"""Application builders."""
 
 from .sock_shop.application import get_sock_shop
 
